c1ce3e4c58ca016ee3c094b2f2fc059ebb3f89bc
Fiantso Harena <fiantsorav@gmail.com>
2025-05-30 19:38:05 +0300

    style: customize tab bar style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,22 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Entypo from "@expo/vector-icons/Entypo";
+import { useColorScheme } from "react-native";
 
 export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "#007FFF",
+        tabBarInactiveTintColor: colorScheme === "dark" ? "#8899A6" : "#657786",
+        tabBarStyle: {
+          backgroundColor: colorScheme === "dark" ? "#15202B" : "#FFFFFF",
+        },
         tabBarLabelStyle: {
-          fontSize: 12,
+          fontSize: 8,
           fontFamily: "Poppins-Medium",
         },
       }}
