720b56402c590dec869b102df673637ca2720ce3
Fiantso Harena <fiantsorav@gmail.com>
2025-05-30 16:03:50 +0300

    chore: set poppins as font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
-import { Stack } from "expo-router";
+import {SplashScreen, Stack} from "expo-router";
+import {useEffect} from "react";
+import {
+    useFonts,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 export default function RootLayout() {
-  return <Stack />;
+    const [fontsLoaded, fontError] = useFonts({
+        "Poppins-Regular": Poppins_400Regular,
+        "Poppins-Medium": Poppins_500Medium,
+        "Poppins-SemiBold": Poppins_600SemiBold,
+        "Poppins-Bold": Poppins_700Bold,
+    });
+
+    useEffect(() => {
+        SplashScreen.preventAutoHideAsync();
+    }, []);
+
+    useEffect(() => {
+        if (fontsLoaded || fontError) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
+    return <Stack/>;
 }
