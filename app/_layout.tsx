fbb5aea431c05bc1d1446d32aba9070192c29ef7
Fiantso Harena <fiantsorav@gmail.com>
2025-05-30 16:06:32 +0300

    chore: prettier configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,34 @@
-import {SplashScreen, Stack} from "expo-router";
-import {useEffect} from "react";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import {
-    useFonts,
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_600SemiBold,
-    Poppins_700Bold,
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 
 export default function RootLayout() {
-    const [fontsLoaded, fontError] = useFonts({
-        "Poppins-Regular": Poppins_400Regular,
-        "Poppins-Medium": Poppins_500Medium,
-        "Poppins-SemiBold": Poppins_600SemiBold,
-        "Poppins-Bold": Poppins_700Bold,
-    });
+  const [fontsLoaded, fontError] = useFonts({
+    "Poppins-Regular": Poppins_400Regular,
+    "Poppins-Medium": Poppins_500Medium,
+    "Poppins-SemiBold": Poppins_600SemiBold,
+    "Poppins-Bold": Poppins_700Bold,
+  });
 
-    useEffect(() => {
-        SplashScreen.preventAutoHideAsync();
-    }, []);
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+  }, []);
 
-    useEffect(() => {
-        if (fontsLoaded || fontError) {
-            SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded, fontError]);
-
-    if (!fontsLoaded && !fontError) {
-        return null;
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
     }
+  }, [fontsLoaded, fontError]);
 
-    return <Stack/>;
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return <Stack />;
 }
