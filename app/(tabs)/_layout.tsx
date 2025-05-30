ae5e327c4d7ee0e93d529e3f9cc5070b5bef9338
Fiantso Harena <fiantsorav@gmail.com>
2025-05-30 16:48:09 +0300

    chore: add icon to tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,6 @@
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function TabLayout() {
   return (
@@ -15,30 +17,37 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Accueil",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="home" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="club"
         options={{
           title: "Vie du Club",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="users" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="student"
         options={{
           title: "Vie Étudiante",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="calendar" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="messages"
         options={{
           title: "Messages",
+          tabBarIcon: ({ color, size }) => <Entypo name="message" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profil",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="user" size={size} color={color} />,
         }}
       />
     </Tabs>
